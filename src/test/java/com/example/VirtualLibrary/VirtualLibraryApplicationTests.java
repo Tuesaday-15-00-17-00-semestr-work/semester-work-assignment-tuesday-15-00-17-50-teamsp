22769440dd6254266c14ac5c0fa062f26ba05508
PeterSnoha <snogofod@gmail.com>
2024-12-04 21:44:33 +0100

    Start fresh

---
+++ src/test/java/com/example/VirtualLibrary/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VirtualLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
